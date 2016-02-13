ac22fbea4aa075ab3db322e329f5b4fd2a9eea94
Tom <stainer.tom@gmail.com>
2016-02-13 20:42:21 +0100

    Initial Commit

---
+++ Solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Solver
+//
+//  Created by Tom Stainer on 13/02/2016.
+//  Copyright (c) 2016 Tom Stainer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
