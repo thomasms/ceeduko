9fb85fa6746f4c64d76196125171fd5192f13eaf
thomasms <stainer.tom@gmail.com>
2018-03-14 21:52:07 +0000

    Added an empty python api - to be implemented

---
+++ source/py/api.cpp
@@ -0,0 +1,8 @@
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pyduko, m) {
+    m.doc() = "Python API for ceeduko solver using pybind11"; // optional module docstring
+    //ToDo: implement
+}
